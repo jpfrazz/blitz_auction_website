34858f418ee6166be8c64e7ed67982876cdb84ae
jpfrazz <jpfrazz@gmail.com>
2026-01-11 18:23:24 -0500

    updated schema

--- backend/migrations/20260111184206_initial-schema.sql
+++ backend/migrations/20260111184206_initial-schema.sql
@@ -1,6 +1,16 @@
-CREATE TYPE versions AS ENUM (
-    '1.0.0'
-)
+CREATE TABLE users (
+    id UUID PRIMARY KEY,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT now()
+);
+
+CREATE TABLE pokemon (
+    id INT NOT NULL,
+    patch_version TEXT NOT NULL,
+    name TEXT NOT NULL,
+    base_stats JSONB NOT NULL,
+
+    PRIMARY KEY (id, patch_version)
+);
 
 CREATE TABLE drafts (
     id TEXT PRIMARY KEY,
@@ -8,39 +18,29 @@ CREATE TABLE drafts (
     status TEXT NOT NULL DEFAULT 'PENDING',
     patch_version TEXT NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
-)
+);
 
 CREATE TABLE auctions (
-    id INT NOT NULL REFERENCES pokemon(id),
+    pokemon_id INT NOT NULL ,
     patch_version TEXT NOT NULL,
     draft_id TEXT NOT NULL REFERENCES drafts(id) ON DELETE CASCADE,
     status TEXT NOT NULL DEFAULT 'PENDING',
     winning_bid INT,
-    winner UUID REFERENCES users(id),
+    winner_id UUID REFERENCES users(id),
     created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT now(),
 
-    PRIMARY KEY (id, draft_id)
-)
+    FOREIGN KEY (pokemon_id, patch_version)
+        REFERENCES pokemon(id, patch_version),
 
-CREATE TABLE pokemon (
-    id INT NOT NULL,
-    patch_version versions NOT NULL,
-    name TEXT NOT NULL,
-    base_stats JSONB NOT NULL,
-
-    PRIMARY KEY (id, patch_version)
-)
-
-CREATE TABLE users (
-    id UUID,
-    created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
-)
+    PRIMARY KEY (pokemon_id, draft_id)
+);
 
 CREATE TABLE players (
-    id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE
-    draft_id TEXT NOT NULL REFERENCES drafts(id) ON DELETE CASCADE
+    id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    draft_id TEXT NOT NULL REFERENCES drafts(id) ON DELETE CASCADE,
     money_remaining INT NOT NULL,
-    is_connected BOOL NOT NULL DEFAULT false
-)
+    is_connected BOOL NOT NULL DEFAULT false,
 
+    PRIMARY KEY (id, draft_id)
+);
