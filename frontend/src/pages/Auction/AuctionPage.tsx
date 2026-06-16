648e1cf5d275a43caf60903df668463c54ed5c28
FranklyNathan <spamemailldsaopqasdfasd@gmail.com>
2026-06-16 00:27:58 -0700

    Hiding Emulator Button

--- frontend/src/pages/Auction/AuctionPage.tsx
+++ frontend/src/pages/Auction/AuctionPage.tsx
@@ -679,7 +679,7 @@ const AuctionPage: React.FC = () => {
                           Submit Results
                         </button>
                       )}
-                      {isLoggedIn && currentUsername === 'franklynathan' && (
+                      {hasRefereeRole && (
                         <button
                           className="button"
                           onClick={() => window.open(`/Emulator/${draft.draft_id}`, '_blank')}
