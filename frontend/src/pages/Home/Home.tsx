72aff34046739ca9838d6c5fced93a13b8ed27a8
FranklyNathan <spamemailldsaopqasdfasd@gmail.com>
2026-07-30 23:18:04 -0700

    v1.0.2

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -163,7 +163,7 @@ const Home = () => {
         />
         <div className={`home-actions ${isLoaded ? 'animate' : ''}`}>
           <div className="home-version-pill">
-            Current Release: v1.0.1
+            Current Release: v1.0.2
           </div>
           <a
             href="/emeraldblitz.bps"
