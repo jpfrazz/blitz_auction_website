282b48595b110a8837fe41288c0809143b1100ed
FranklyNathan <spamemailldsaopqasdfasd@gmail.com>
2026-09-09 08:00:03 -0700

    Redeploy

--- frontend/src/shared/components/Header.tsx
+++ frontend/src/shared/components/Header.tsx
@@ -285,7 +285,7 @@ function Header() {
                 Auction Setup
               </Link>
                 <Link to="/Draft1v1Setup" className="navButton navDropdownItem" onClick={handleNavLinkClick} target={linkTarget} rel={linkRel}>
-                  1v1 Setup
+                  1v1 Setup (New!)
                 </Link>
               <Link to="/LobbyViewer" className="navButton navDropdownItem" onClick={handleNavLinkClick} target={linkTarget} rel={linkRel}>
                 Lobby Viewer
